66c1b95d60394bd48a5b668984480eebd5b9eebe
myllenavieira <vieirasilvamyllena@gmail.com>
2022-08-03 21:50:22 -0300

    Exercícios para prática 012

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = 10//seu numero aqui
 
-if(){
-    console.log("sua mensagem aqui");
+if(num%2===0){
+    console.log(`O seu número é ${num}, e ele é um número par`);
+}else{
+    console.log(`O seu número é ${num}, e ele é um número ímpar`)
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Qual a sua nacionalidade?');
+
+if(nacionalidade.toLowerCase() == 'brasileira'){
+    console.log(`Sua nacionalidade é brasileira.`);
+} else if(nacionalidade.toLowerCase() == 'argentina'){
+    console.log(`Sua nacionalidade é argentina.`);
+} else if(nacionalidade.toLowerCase() == 'uruguaia'){
+    console.log(`Sua nacionalidade é uruguaia.`);
+} else if(nacionalidade.toLowerCase() == 'chilena'){
+    console.log(`Sua nacionalidade é chilena.`);
+} else if(nacionalidade.toLowerCase() == 'colombiana' ){
+    console.log(`Sua nacionalidade é colombiana.`);
+} else{
+    console.log(`Sua nacionalidade ${nacionalidade} não foi encontrada.`);
+}
